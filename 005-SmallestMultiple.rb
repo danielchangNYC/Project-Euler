3b20a9bcf78de84b0e663145f9c59095166b5f3a
Daniel Chang <daniel.chang85@gmail.com>
2014-02-06 18:46:05 -0500

    005-smallestmultiple done

--- 005-SmallestMultiple.rb
+++ 005-SmallestMultiple.rb
@@ -1,3 +1,17 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+factors = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+n = 120452800
+searching = true
+
+while searching
+  puts "Current: #{n}"
+  if factors.select { |i| n % i != 0 }.empty?
+    searching = false
+    puts "The number is #{n}"
+  end
+  n += 20
+end
