d9523e4b0ded6cf47fe761b884c4234703207474
Daniel Chang <daniel.chang85@gmail.com>
2014-02-06 18:57:03 -0500

    006 Sum of Squares done

--- 006-SumSquareDifference.rb
+++ 006-SumSquareDifference.rb
@@ -7,3 +7,5 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+puts ((101*50)^2) - (1..100).inject(1){ |result, n| result + n^2}
